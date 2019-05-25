35f75706d2d6e6ba72168cacd23a6533bc80c557
Tang1705 <17301138@bjtu.edu.cn>
2019-05-25 22:50:04 +0800

    规范Theme父类变量名并修改为单例

---
+++ Classes/Theme.cpp
@@ -0,0 +1,9 @@
+#include "Theme.h"
+
+Theme* Theme::instance = nullptr;
+
+Theme* Theme::getInstance()
+{
+	if (instance == nullptr) instance = new Theme();
+	return instance;
+}
--- Classes/Theme.h
+++ Classes/Theme.h
@@ -7,39 +7,47 @@ using std::string;
 class Theme
 {
 public:
-	const string ICON_SET_ZERO;
-	const string ICON_SET_ONE;
-	const string ICON_SET_TWO;
-	const string ICON_SET_THREE;
-	const string ICON_SET_FOUR;
-	const string ICON_SET_FIVE;
-	const string ICON_SET_SIX;
-	const string ICON_SET_SEVEN;
-	const string ICON_SET_EIGHT;
-	const string ICON_SET_NINE;
-	const string LOADING_SCENE_LABEL_PRESS;
-	const string LOADING_SCENE_LABEL_TITLE;
-	const string LOADING_SCENE_BACKGROUND;
-	const string MENU_SCENE_BACKGROUND;
-	const string MENU_SCENE_GAME_BUTTON_NORMAL;
-	const string MENU_SCENE_GAME_BUTTON_SELECTED;
-	const string MENU_SCENE_GAME_BUTTON_DISABLED;
-	const string MENU_SCENE_RANK_BUTTON_NORMAL;
-	const string MENU_SCENE_RANK_BUTTON_SELECTED;
-	const string MENU_SCENE_RANK_BUTTON_DISABLED;
-	const string MENU_SCENE_STORE_BUTTON_NORMAL;
-	const string MENU_SCENE_STORE_BUTTON_SELECTED;
-	const string MENU_SCENE_STORE_BUTTON_DISABLED;
-	const string LEVEL_SCENE_VISION;
-	const string LEVEL_SCENE_PROSPECT;
-	const string LEVEL_SCENE_GAME_BUTTON_NORMAL;
-	const string LEVEL_SCENE_GAME_BUTTON_SELECTED;
-	const string LEVEL_SCENE_GAME_BUTTON_DISABLED;
-	const string GAME_SCENE_BACKGROUND;
-	const string GAME_SCENE_LEVEL_SPRITE_BACKGROUND;
-	const string GAME_SCENE_PROMPT_BUTTON_NORMAL;
-	const string GAME_SCENE_PROMPT_BUTTON_SELECTED;
-	const string GAME_SCENE_PROMPT_BUTTON_DISABLED;
+#pragma region Theme Const String
+	const string iconSetZero;
+	const string iconSetOne;
+	const string iconSetTwo;
+	const string iconSetThree;
+	const string iconSetFour;
+	const string iconSetFive;
+	const string iconSetSix;
+	const string iconSetSeven;
+	const string iconSetEight;
+	const string iconSetNine;
+	const string loadingSceneLabelPress;
+	const string loadingSceneLabelTitle;
+	const string loadingSceneBackground;
+	const string menuSceneBackground;
+	const string menuSceneGameButtonNormal;
+	const string menuSceneGameButtonSelected;
+	const string menuSceneGameButtonDisabled;
+	const string menuSceneRankButtonNormal;
+	const string menuSceneRankButtonSelected;
+	const string menuSceneRankButtonDisabled;
+	const string menuSceneStoreButtonNormal;
+	const string menuSceneStoreButtonSelected;
+	const string menuSceneStoreButtonDisabled;
+	const string levelSceneVision;
+	const string levelSceneProspect;
+	const string levelSceneGameButtonNormal;
+	const string levelSceneGameButtonSelected;
+	const string levelSceneGameButtonDisabled;
+	const string gameSceneBackground;
+	const string gameSceneLevelSpriteBackground;
+	const string gameScenePromptButtonNormal;
+	const string gameScenePromptButtonSelected;
+	const string gameScenePromptButtonDisabled;
+#pragma endregion
+
+	static Theme* getInstance();
+
+private:
+	static Theme* instance;
+
 };
 
 #endif
