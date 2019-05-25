9fb910b58021dbf764934fbad42d680f87a6494a
董舜尧 <dongshunyao@qq.com>
2019-05-25 19:42:40 +0800

    工具添加哈希函数

--- Classes/util.cpp
+++ Classes/util.cpp
@@ -2,6 +2,21 @@
 
 long long Util::getStringHash(const string& str)
 {
-	// TODO Hash
-	return -1;
+	const int length = str.length();
+	vector<int> hash(length + 10);
+
+	hash[0] = static_cast<long long>(str[0]) % MOD;
+	for (auto i = 1; i < str.length(); i++) hash[i] = (hash[i - 1] * BASE + static_cast<long long>(str[i])) % MOD;
+
+	return (hash[length - 1] - length * BASE + MOD) % MOD;
+}
+
+long long Util::getStringHash(vector<string> str)
+{
+	sort(str.begin(), str.end());
+
+	long long hash = 0;
+	for (const auto& it : str) hash = (hash + getStringHash(it)) % MOD;
+
+	return hash;
 }
--- Classes/util.h
+++ Classes/util.h
@@ -2,12 +2,22 @@
 #define __UTIL_H__
 
 #include <string>
+#include <vector>
+#include <algorithm>
+
 using std::string;
+using std::vector;
+using std::sort;
 
 namespace Util
 {
-	long long getStringHash(const string& str);
+#pragma region Hash
+	const long long BASE = 233;
+	const long long MOD = 1000000000 + 9;
 
+	long long getStringHash(const string& str);
+	long long getStringHash(vector<string> str);
+#pragma endregion
 };
 
 #endif
