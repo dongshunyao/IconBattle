0d487e6fb995fb05f8c53d74ef81a635fc95e848
Machenike <870096160@qq.com>
2019-05-28 22:46:07 +0800

    添加GameScene鼠标监听器

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -21,7 +21,6 @@ bool GameScene::init()
 
 void GameScene::initComponents()
 {
-
 #pragma  region Init GameScene
 
 	// 添加背景图片
@@ -82,7 +81,7 @@ void GameScene::initComponents()
 
 	// 提示按钮
 	auto hintButton = ui::Button::create(theme->gameSceneHintButtonNormal, theme->gameSceneHintButtonSelected,
-		theme->gameSceneHintButtonDisabled);
+	                                     theme->gameSceneHintButtonDisabled);
 	hintButton->setPosition(Point(375, 540));
 	hintButton->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type)
 	{
@@ -103,8 +102,81 @@ void GameScene::initComponents()
 
 #pragma  region addMouseListener
 	{
-		
+		auto mouseListener = EventListenerMouse::create();
+		// 鼠标按下
+		mouseListener->onMouseDown = [&](Event* event)
+		{
+			if (boardLocked)return;
+
+			const auto e = dynamic_cast<EventMouse*>(event);
+			// 只按下左键为有效操作
+			if (e->getMouseButton() == EventMouse::MouseButton::BUTTON_LEFT)
+			{
+				auto cursorX = e->getCursorX();
+				auto cursorY = e->getCursorY();
+
+				// TODO: 选中标识动画
+				// 还未选中过方块
+				if (selectedBlockF.first == -1)
+					selectedBlockF = getIndex(Pii(cursorX, cursorY));
+				//已经选中一个方块
+				else if(selectedBlockF.first != -1 && selectedBlockS.first == -1)
+				{
+					selectedBlockS = getIndex(Pii(cursorX, cursorY));
+					// 判断第二方块是否相邻
+					switch (selectedBlockS.first - selectedBlockF.first)
+					{
+					case 1: case -1:
+						{
+							// 不相邻，现选择的方块变为第一块
+							if (selectedBlockF.second != selectedBlockS.second)
+							{
+								selectedBlockF = getIndex(Pii(cursorX, cursorY));
+								selectedBlockS = { -1,-1 };
+							}
+							break;
+						}
+						
+					case 0:
+						{
+							// 不相邻，现选择的方块变为第一块
+							if ((selectedBlockF.second - selectedBlockS.second != -1)
+								|| (selectedBlockF.second - selectedBlockS.second != 1))
+							{
+								selectedBlockF = getIndex(Pii(cursorX, cursorY));
+								selectedBlockS = { -1,-1 };
+							}
+							break;
+						}
+
+					// 不相邻，现选择的方块变为第一块
+					default:
+						{
+							selectedBlockF = getIndex(Pii(cursorX, cursorY));
+							selectedBlockS = { -1,-1 };
+							break;
+						}
+					}
+				}
+			}
+
+		};
+
+		// 鼠标弹起
+		mouseListener->onMouseUp = [&](Event* event)
+		{
+			const auto e = dynamic_cast<EventMouse*>(event);
+			// 只左键弹起有效
+			if (e->getMouseButton() == EventMouse::MouseButton::BUTTON_LEFT)
+			{
+				// 已经选中两块则尝试交换，否则不进行操作
+				if (selectedBlockF.first != -1 && selectedBlockS.first != -1)
+				{
+					trySwap(selectedBlockF, selectedBlockS);
+				}
+			}
+		};
 	}
 
-#pragma endregion 
+#pragma endregion
 }
