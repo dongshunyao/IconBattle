5fa629ab8087e5f7c47bd0b14d7b9cc6e417cfaf
Machenike <870096160@qq.com>
2019-06-05 22:17:15 +0800

    修缮NetworkShare

--- Classes/NetworkShare.cpp
+++ Classes/NetworkShare.cpp
@@ -3,7 +3,7 @@
 // bool NetworkShare::share(bool isClassical)
 // bool NetworkShare::share(bool isClassical,int level,int score)
 // bool NetworkShare::share(bool isClassical,int score,int rank) 
-bool NetworkShare::share(vector<string>& info, const int target)
+bool NetworkShare::share(const bool isClassical, const string title, const int target)
 {
 	switch (target)
 	{
@@ -11,8 +11,12 @@ bool NetworkShare::share(vector<string>& info, const int target)
 		{
 			auto qq = QQ_SHARE_URL;
 			qq += "&desc=";
-			qq += info[0];
-			qq += QQ_PIC;
+			qq += title;
+			if(isClassical)
+				qq += QQ_PIC_CLASSICAL;
+			else
+				qq += QQ_PIC;
+			
 			Application::getInstance()->openURL(qq);
 			break;
 		}
@@ -20,8 +24,12 @@ bool NetworkShare::share(vector<string>& info, const int target)
 		{
 			auto weibo = WEIBO_SHARE_URL;
 			weibo += "title=";
-			weibo += info[0];
-			weibo += WEIBO_PIC;
+			weibo += title;
+			if (isClassical)
+				weibo += WEIBO_PIC_CLASSICAL;
+			else
+				weibo += WEIBO_PIC;
+
 			Application::getInstance()->openURL(weibo);
 			break;
 		}
@@ -29,8 +37,12 @@ bool NetworkShare::share(vector<string>& info, const int target)
 		{
 			auto renren = WEIBO_SHARE_URL;
 			renren += "&title=";
-			renren += info[0];
-			renren += RENREN_PIC;
+			renren += title;
+			if (isClassical)
+				renren += RENREN_PIC_CLASSICAL;
+			else
+				renren += RENREN_PIC;
+
 			Application::getInstance()->openURL(RENREN_SHARE_URL);
 			break;
 		}
@@ -38,13 +50,72 @@ bool NetworkShare::share(vector<string>& info, const int target)
 		{
 			auto douban = WEIBO_SHARE_URL;
 			douban += "name=";
-			douban += info[0];
-			douban += DOUBAN_PIC;
+			douban += title;
+			if (isClassical)
+				douban += DOUBAN_PIC_CLASSICAL;
+			else
+				douban += DOUBAN_PIC;
+
 			Application::getInstance()->openURL(douban);
 			break;
 		}
 	default:
 		return false;
 	}
+	return true;
+}
+
+bool NetworkShare::sharePractice(const bool isClassical, const int target)
+{
+	string temp;
+	if (isClassical)
+	{
+		temp = "I'm playing IconBattle practicing mode, come and join me.";
+		share(isClassical, temp, target);
+		return true;
+	}
+
+	temp = "I'm playing IconBattle advanced mode, come and join me.";
+	share(isClassical, temp, target);
+	return true;
+}
+
+bool NetworkShare::shareLevel(const bool isClassical, const int target, const int level)
+{
+	string temp;
+	if (isClassical)
+	{
+		temp = "I've passed level ";
+		temp += std::to_string(level);
+		temp += " of IconBattle practicing mode. That would be nice.";
+		share(isClassical, temp, target);
+		return true;
+	}
+
+	temp = "I've passed level ";
+	temp += std::to_string(level);
+	temp += " of IconBattle advanced mode. Awesome!";
+	share(isClassical, temp, target);
+	return true;
+}
+
+bool NetworkShare::shareRank(const bool isClassical, const int target, const int score, const int rank)
+{
+	string temp;
+	if (isClassical)
+	{
+		temp = "I've got ";
+		temp += std::to_string(score);
+		temp += " points of IconBattle practicing mode and ranked No.";
+		temp += std::to_string(rank);
+		share(isClassical, temp, target);
+		return true;
+	}
+
+	temp = "I've got ";
+	temp += std::to_string(score);
+	temp += " points of IconBattle advanced mode and ranked No.";
+	temp += std::to_string(rank);
+	share(isClassical, temp, target);
 	return true;
 }
--- Classes/NetworkShare.h
+++ Classes/NetworkShare.h
@@ -30,13 +30,24 @@ namespace NetworkShare
 #pragma endregion
 
 #pragma region Share Pic Url
+	// TODO: 制作并上传分享图片
+	const string QQ_PIC_CLASSICAL = "&pics=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg&flash=&site=&style=201&width=32&height=3";
 	const string QQ_PIC = "&pics=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg&flash=&site=&style=201&width=32&height=3";
-	const string WEIBO_PIC = "&pic=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg&flash=&site=&style=201&width=32&height=3";
-	const string RENREN_PIC = "&pics=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg&flash=&site=&style=201&width=32&height=3";
-	const string DOUBAN_PIC = "&pics=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg&flash=&site=&style=201&width=32&height=3";
+
+	const string WEIBO_PIC_CLASSICAL = "&pic=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg";
+	const string WEIBO_PIC = "&pic=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg";
+
+	const string RENREN_PIC_CLASSICAL = "&pic=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg";
+	const string RENREN_PIC = "&pic=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg";
+
+	const string DOUBAN_PIC_CLASSICAL = "&image=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg";
+	const string DOUBAN_PIC = "&image=https://i.loli.net/2019/05/19/5ce15be219ef569212.jpg";
 #pragma endregion 
 
-	bool share(vector<string>& info, const int target);
+	bool share(bool isClassical, const string title, const int target);
+	bool sharePractice(bool isClassical, const int target);
+	bool shareLevel(bool isClassical, const int target, const int level);
+	bool shareRank(bool isClassical, const int target, const int score, const int rank);
 };
 
 #endif
